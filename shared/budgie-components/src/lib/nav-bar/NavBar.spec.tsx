4c060cdc3f943a1478cffa853ea08805498da6d8
James_AF_Fitzsimons <u21516741@tuks.co.za>
2024-05-29 20:01:28 +0200

    chore(main):initital setup things

--- shared/budgie-components/src/lib/nav-bar/NavBar.spec.tsx
+++ shared/budgie-components/src/lib/nav-bar/NavBar.spec.tsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
 
-import Navbar from './navbar';
+import NavBar from './NavBar';
 
-describe('Navbar', () => {
+describe('NavBar', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Navbar />);
+    const { baseElement } = render(<NavBar />);
     expect(baseElement).toBeTruthy();
   });
 });
