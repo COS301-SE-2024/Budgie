afffe2e0704cb4e1408cbfebaca036d1d5ab4cdc
ArrJayOh <160711869+RebeccaOosthuizen@users.noreply.github.com>
2024-05-31 15:48:11 +0200

    Basic navbar structure

--- shared/budgie-components/src/lib/navbar/navbar.tsx
+++ shared/budgie-components/src/lib/navbar/navbar.tsx
@@ -1,12 +1,21 @@
 'use client';
 
-import'./navbar.module.css';
+import styles from './navbar.module.css';
 
 /* eslint-disable-next-line */
 export interface NavbarProps {}
 
 export function Navbar(props: NavbarProps) {
-    return <div className="sidebar">Hello Navbar test!</div>;
+    return <>
+    <div className={styles.sidebar}>
+            <ul className={styles.navList}>
+                <li className={styles.navItem}>Home</li>
+                <li className={styles.navItem}>Dashboard</li>
+                <li className={styles.navItem}>Settings</li>
+                <li className={styles.navItem}>Logout</li>
+            </ul>
+        </div>
+    </>
 }
 
 export default Navbar;
