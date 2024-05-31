44b7c3f09da725ec5b6117aa476bf10adfafe1ea
James_AF_Fitzsimons <u21516741@tuks.co.za>
2024-05-31 17:29:30 +0200

    feat(SignInModal):sign in modal frontend started

--- shared/budgie-components/src/lib/sign-in-modal/SignInModal.tsx
+++ shared/budgie-components/src/lib/sign-in-modal/SignInModal.tsx
@@ -1,12 +1,32 @@
 'use client';
 
-import styles from './SignInModal.module.css';
+import './SignInModal.module.css';
+import { Fragment, useState } from 'react';
+import Image from 'next/image';
+import logo from '../../../public/images/BudgieNoBG.png';
 
 /* eslint-disable-next-line */
 export interface SignInModalProps {}
 
 export function SignInModal(props: SignInModalProps) {
-  return <div className="bg-indigo-500 p-4">Hello Tailwind test!</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <>
+      <div className="bg-BudgieBlue w-[794px] h-[521px] rounded-[61px] m-5">
+        <div className="relative bg-BudgieWhite w-[397px] h-[521px] rounded-[60px] rounded-tr-none rounded-br-none ">
+          <div className="h-[55px] w-[55px]">
+            <Image src={logo} alt="Logo"></Image>
+          </div>
+          <p className=" font-TripSans text-3xl text-BudgieBlue">
+            Welcome to Budgie
+          </p>
+          <form></form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default SignInModal;
