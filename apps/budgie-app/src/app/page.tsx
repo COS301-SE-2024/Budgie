65f1a3c1570d28b015297959642b7ca561118a7a
James_AF_Fitzsimons <u21516741@tuks.co.za>
2024-05-31 11:12:07 +0200

    chore(main):added tailwind

--- apps/budgie-app/src/app/page.tsx
+++ apps/budgie-app/src/app/page.tsx
@@ -1,5 +1,6 @@
 import styles from './page.module.css';
+import { SignInModal } from '@capstone-repo/shared/budgie-components';
 
 export default function Index() {
-  return <div>Hello</div>;
+  return <SignInModal></SignInModal>;
 }
---
+++ shared/budgie-components/src/lib/sign-in-modal/SignInModal.tsx
@@ -0,0 +1,10 @@
+import styles from './SignInModal.module.css';
+
+/* eslint-disable-next-line */
+export interface SignInModalProps {}
+
+export function SignInModal(props: SignInModalProps) {
+  return <div className="bg-indigo-500 p-4">Hello Tailwind test!</div>;
+}
+
+export default SignInModal;
