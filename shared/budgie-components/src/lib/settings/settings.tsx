49feb28e6876e85ce04ae0e84ad33628149b6602
u22486985 <u22486985@tuks.co.za>
2024-05-31 17:28:42 +0200

    added delete button
    added delete button

--- shared/budgie-components/src/lib/settings/settings.tsx
+++ shared/budgie-components/src/lib/settings/settings.tsx
@@ -1,12 +1,21 @@
+'use client';
 import styles from './settings.module.css';
 
 /* eslint-disable-next-line */
 export interface SettingsProps {}
 
 export function Settings(props: SettingsProps) {
+
+  const handleDeleteAccount = () => {
+    // Implement the logic to delete the account here
+    alert('Account deleted!');
+  };
+
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Settings!</h1>
+      <button className={styles.deleteButton} onClick={handleDeleteAccount}>
+        <div className={styles.settingsTitle}>Delete Account</div>
+      </button>
     </div>
   );
 }
