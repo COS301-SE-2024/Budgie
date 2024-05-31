087cb62413b660692d50d6e89c68f66018a56c0c
u22486985 <u22486985@tuks.co.za>
2024-05-31 19:21:12 +0200

    added delete confirmation box
    added delete confirmation box

--- shared/budgie-components/src/lib/settings/settings.tsx
+++ shared/budgie-components/src/lib/settings/settings.tsx
@@ -1,21 +1,43 @@
 'use client';
 import styles from './settings.module.css';
+import React, { useState } from 'react';
 
-/* eslint-disable-next-line */
 export interface SettingsProps {}
 
 export function Settings(props: SettingsProps) {
 
-  const handleDeleteAccount = () => {
-    // Implement the logic to delete the account here
+  const [isPopupVisible, setPopupVisible] = useState(false);
+  const handleDeleteClick = () => {
+    setPopupVisible(true);
+  };
+
+  const handleClosePopup = () => {
+    setPopupVisible(false);
+  };
+  const handleConfirmDelete = () => {
+    // Add logic to delete the account here
     alert('Account deleted!');
+    setPopupVisible(false);
   };
 
   return (
     <div className={styles['container']}>
-      <button className={styles.deleteButton} onClick={handleDeleteAccount}>
+      <button className={styles.deleteButton} onClick={handleDeleteClick}>
         <div className={styles.settingsTitle}>Delete Account</div>
       </button>
+      {isPopupVisible && (
+        <div className= {styles.popupOverlay}>
+          <div className={styles.popupContent}>
+            <p>Are you sure you want to delete your account?</p>
+            <button className={styles.confirmButton} onClick={handleConfirmDelete}>
+              Yes
+            </button>
+            <button className={styles.cancelButton} onClick={handleClosePopup}>
+              No
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
