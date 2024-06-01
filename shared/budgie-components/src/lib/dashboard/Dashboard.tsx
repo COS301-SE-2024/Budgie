3299c6186653373dd6f84e462aa8242bcfa99c4f
u22486985 <u22486985@tuks.co.za>
2024-06-01 11:40:25 +0200

    added upload bank statement button
    added upload bank statement button to upload a csv

--- shared/budgie-components/src/lib/dashboard/Dashboard.tsx
+++ shared/budgie-components/src/lib/dashboard/Dashboard.tsx
@@ -1,14 +1,43 @@
 'use client';
 import styles from './Dashboard.module.css';
 import "../../root.css";
+import React, { useRef } from 'react';
 
 /* eslint-disable-next-line */
-export interface DashboardProps {}
+export interface DashboardProps {
+  onFileUpload: (file: File) => void;
+}
 
 export function Dashboard(props: DashboardProps) {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleButtonClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      const file = files[0];
+      props.onFileUpload(file);
+    }
+  };
+
   return (
     <div className='mainPage'>
       <span className='pageTitle'>Dashboard</span>
+      <button className={styles.uploadButton} onClick={handleButtonClick}>
+        <div className={styles.uploadButton}>Upload Bank Statement</div>
+      </button>
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: 'none' }}
+        accept=".csv"
+        onChange={handleFileChange}
+      />
     </div>
   );
 }
