03e21550e50eadb5b6478df49f682c1c9d27c938
James_AF_Fitzsimons <u21516741@tuks.co.za>
2024-06-02 12:42:29 +0200

    feat(feat/integration_1): modal changes sign in

--- shared/budgie-components/src/lib/sign-in-modal/SignInModal.tsx
+++ shared/budgie-components/src/lib/sign-in-modal/SignInModal.tsx
@@ -15,6 +15,16 @@ export function SignInModal(props: SignInModalProps) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleEmailChange = (e: React.SyntheticEvent<HTMLInputElement>) => {
+    setEmail((e.target as HTMLInputElement).value);
+  };
+
+  const handlePasswordChange = (e: React.SyntheticEvent<HTMLInputElement>) => {
+    setPassword((e.target as HTMLInputElement).value);
+  };
+
+  const handleLoginClick = () => {};
+
   return (
     <>
       <div className="bg-BudgieBlue w-[794px] h-[521px] rounded-[61px]">
@@ -32,20 +42,25 @@ export function SignInModal(props: SignInModalProps) {
                 id="email"
                 type="text"
                 placeholder="Email"
+                value={email}
+                onChange={handleEmailChange}
               ></input>
             </div>
             <div className="pt-4">
               <input
                 className="appearance-none text-lg w-72 h-10 font-TripSans font-normal pl-3 bg-BudgieGrayLight border rounded-[10px] focus:outline-none focus:shadow"
                 id="password"
-                type="text"
+                type="password"
                 placeholder="Password"
+                value={password}
+                onChange={handlePasswordChange}
               ></input>
             </div>
             <div className="flex flex-col justify-start pt-6 items-center">
               <button
                 className=" font-TripSans font-medium rounded-[25px] w-36 h-10 bg-BudgieBlue text-BudgieWhite"
                 type="button"
+                onClick={handleLoginClick}
               >
                 Log In
               </button>
