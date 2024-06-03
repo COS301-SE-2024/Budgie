722fff84a560b878831b87f84dd5c8d09258db16
James_AF_Fitzsimons <u21516741@tuks.co.za>
2024-06-03 11:08:46 +0200

    fix(feat/testing):fix test missing react import

--- shared/budgie-components/src/lib/navbar/navbar.spec.tsx
+++ shared/budgie-components/src/lib/navbar/navbar.spec.tsx
@@ -4,7 +4,14 @@ import React from 'react';
 
 describe('Navbar', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Navbar />);
+    const { baseElement } = render(
+      <Navbar
+        onNavigate={function (page: string): void {
+          throw new Error('Function not implemented.');
+        }}
+        selectedItem={''}
+      />
+    );
     expect(baseElement).toBeTruthy();
   });
 });
--- shared/budgie-components/src/lib/settings/settings.spec.tsx
+++ shared/budgie-components/src/lib/settings/settings.spec.tsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 
 import Settings from './settings';
+import React from 'react';
 
 describe('Settings', () => {
   it('should render successfully', () => {
