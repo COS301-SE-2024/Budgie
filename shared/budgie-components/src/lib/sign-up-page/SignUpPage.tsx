9a6cdc9e626e9d965a0c2eb4dbe753fd8777b6d4
James_AF_Fitzsimons <u21516741@tuks.co.za>
2024-06-15 14:27:25 +0200

    feat/(signup-changes):small fixes

--- shared/budgie-components/src/lib/sign-up-page/SignUpPage.tsx
+++ shared/budgie-components/src/lib/sign-up-page/SignUpPage.tsx
@@ -9,7 +9,6 @@ export interface SignUpPageProps {}
 export function SignUpPage(props: SignUpPageProps) {
   return (
     <>
-      <div></div>
       <div className="z-20 absolute w-screen h-screen backdrop-blur-sm"></div>
       <div className="absolute z-50 left-[calc(50vw-397px)] top-[calc(50vh-260px)]">
         <SignUpModal></SignUpModal>
