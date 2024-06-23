77894d7d7ff95819d6f6b6bcb54b5c493c236ffe
James_AF_Fitzsimons <u21516741@tuks.co.za>
2024-06-23 11:57:14 +0200

    chore(dev):gitignorechanges

--- shared/budgie-components/jest.setup.js
+++ shared/budgie-components/jest.setup.js
@@ -1,4 +1,11 @@
-import { mockFirebaseAuth, mockNextImage } from './src/mocks/globalMocks';
+import {
+  mockFirebaseAuth,
+  mockNextImage,
+  mockFirebaseFunctions,
+  mockNextRouter,
+} from './src/mocks/globalMocks';
 
 jest.mock('firebase/auth', () => mockFirebaseAuth);
 jest.mock('next/image', () => mockNextImage.Image);
+jest.mock('firebase/functions', () => mockFirebaseFunctions);
+jest.mock('next/navigation', () => mockNextRouter);
