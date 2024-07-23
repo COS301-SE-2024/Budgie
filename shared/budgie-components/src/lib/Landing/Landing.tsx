476735a7610ef376e8ea29af13ca89dbf253eebe
kabeloChuene <68307899+kabeloChuene@users.noreply.github.com>
2024-07-23 23:47:06 +0200

    rough landing pages styling
    Still trying to get the desired styling done with waves

--- shared/budgie-components/src/lib/Landing/Landing.tsx
+++ shared/budgie-components/src/lib/Landing/Landing.tsx
@@ -24,47 +24,50 @@ export function Landing(props: LandingProps) {
           </div>
         </>
       )}
-      <div className="relative flex items-center justify-center min-h-screen">
-        <div className="absolute inset-0 bg-gradient-to-b " />
-        {!showSignInModal && (
-          <>
-            <div className="relative z-10 text-center">
-              <div className="flex flex-col items-center w-[560px] h-[332px]">
-                <h1 className="font-TripSans font-extrabold text-8xl md:text-[128px] text-BudgieBlue absolute top-24 opacity-[13%]">
-                  Budgie
-                </h1>
-                <h1 className="font-TripSans font-extrabold text-8xl md:text-[128px] text-BudgieBlue absolute top-10 opacity-5">
-                  Budgie
-                </h1>
-                <h1 className="font-TripSans font-extrabold text-8xl md:text-[128px] text-BudgieBlue absolute bottom-12 ">
-                  Budgie
-                </h1>
-                <p className="font-TripSans font-extrabold text-xl md:text-1x4 text-BudgieBlue mt-4 absolute bottom-9 right-[28px]">
-                  Financial Wingman
-                </p>
-              </div>
-            </div>
-            <button
-              onClick={() => {
-                setShowSignInModal(!showSignInModal);
-              }}
-              className=" text-lg absolute top-4 right-4 w-[116px] h-[47px] font-TripSans font-bold bg-white hover:bg-BudgieBlue hover:text-BudgieWhite text-BudgieBlue border-4 border-BudgieBlue transition-colors ease-in hover:border-BudgieGrayLight rounded-full "
-            >
-              Sign In
-            </button>
-          </>
-        )}
-        <div className="absolute bottom-4 w-full text-center">
+      <div className={styles.landingContainer}>
+        <div className={styles.waveTop}></div>
+        <div className={styles.waveBottom}></div>
+        <div className="relative flex items-center justify-center min-h-screen">
+          <div className="absolute inset-0 bg-gradient-to-b " />
           {!showSignInModal && (
-            <button className="text-blue-950 font-semibold flex items-center justify-center mx-auto">
-              <span className="text-xl font-TripSans font-bold ml-4 animate-bounce">
-                Learn More &darr;
-              </span>
-            </button>
+            <>
+              <div className="relative z-10 text-center">
+                <div className="flex flex-col items-center w-[560px] h-[332px]">
+                  <h1 className="font-TripSans font-extrabold text-8xl md:text-[128px] text-BudgieBlue absolute top-24 opacity-[13%]">
+                    Budgie
+                  </h1>
+                  <h1 className="font-TripSans font-extrabold text-8xl md:text-[128px] text-BudgieBlue absolute top-10 opacity-5">
+                    Budgie
+                  </h1>
+                  <h1 className="font-TripSans font-extrabold text-8xl md:text-[128px] text-BudgieBlue absolute bottom-12 ">
+                    Budgie
+                  </h1>
+                  <p className="font-TripSans font-extrabold text-xl md:text-1x4 text-BudgieBlue mt-4 absolute bottom-9 right-[28px]">
+                    Financial Wingman
+                  </p>
+                </div>
+              </div>
+              <button
+                onClick={() => {
+                  setShowSignInModal(!showSignInModal);
+                }}
+                className=" text-lg absolute top-4 right-4 w-[116px] h-[47px] font-TripSans font-bold bg-white hover:bg-BudgieBlue hover:text-BudgieWhite text-BudgieBlue border-4 border-BudgieBlue transition-colors ease-in hover:border-BudgieGrayLight rounded-full "
+              >
+                Sign In
+              </button>
+            </>
           )}
+          <div className="absolute bottom-4 w-full text-center">
+            {!showSignInModal && (
+              <button className="text-blue-950 font-semibold flex items-center justify-center mx-auto">
+                <span className="text-xl font-TripSans font-bold ml-4 animate-bounce">
+                  Learn More &darr;
+                </span>
+              </button>
+            )}
+          </div>
         </div>
       </div>
-      <div>Thus is more content</div>
     </>
   );
 }
