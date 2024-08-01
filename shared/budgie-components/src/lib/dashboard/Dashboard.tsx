5730a2344c81eb820423649ed92cef8af5b1da4c
ArrJayOh <160711869+RebeccaOosthuizen@users.noreply.github.com>
2024-08-01 15:40:30 +0200

    Account Change Dropdown Functionality

--- shared/budgie-components/src/lib/dashboard/Dashboard.tsx
+++ shared/budgie-components/src/lib/dashboard/Dashboard.tsx
@@ -5,7 +5,6 @@ import { collection, query, where, getDocs } from 'firebase/firestore';
 import AllTransactionsView from '../all-transactions-view/AllTransactionsView';
 import MonthlyTransactionsView from '../monthly-transactions-view/MonthlyTransactionsView';
 import { UserContext } from '@capstone-repo/shared/budgie-components';
-import { getAuth } from 'firebase/auth';
 
 import '../../root.css';
 import styles from './Dashboard.module.css';
@@ -16,50 +15,61 @@ export function Dashboard(props: DashboardProps) {
 
   const [viewMode, setViewMode] = useState('monthly');
   const user = useContext(UserContext);
-  const [aliases, setAliases] = useState<string[]>([]);
+  const [accountOptions, setAccountOptions] = useState<{ alias: string, accountNumber: string }[]>([]);
   const [currentAccountNumber, setCurrentAccountNumber] = useState<string>('');
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
   const [Data, setData] = useState<any>(null);
+  const [selectedAlias, setSelectedAlias] = useState<string>('');
 
   useEffect(() => {
     const fetchAliases = async () => {
       try {
         const q = query(collection(db, 'accounts'), where('uid', '==', user.uid));
         const querySnapshot = await getDocs(q);
-        const aliasList = querySnapshot.docs.map(doc => doc.data().alias);
-        setAliases(aliasList);
-        const accountNumberList = querySnapshot.docs.map(doc => doc.data().account_number);
-        setCurrentAccountNumber(accountNumberList[0])
-        //console.log(currentAccountNumber);
+        const accountOptionsList = querySnapshot.docs.map(doc => ({
+          alias: doc.data().alias,
+          accountNumber: doc.data().account_number
+        }));
+        setAccountOptions(accountOptionsList);
+
+        if (accountOptionsList.length > 0) {
+          setSelectedAlias(accountOptionsList[0].alias);
+          setCurrentAccountNumber(accountOptionsList[0].accountNumber);
+        }
       } catch (error) {
         console.error("Error fetching aliases: ", error);
       }
     };
 
     fetchAliases();
-  });
+  }, [user.uid]);
 
   useEffect(() => {
     const getYearlyTransactions = async () => {
       try {
-        const q = query(collection(db, `transaction_data_${currentYear}`), where('uid', '==', user.uid), where('account_number', '==', currentAccountNumber));
-        const querySnapshot = await getDocs(q);
-        const transactionList = querySnapshot.docs.map(doc => doc.data());
-        setData(transactionList[0]);
+        if (currentAccountNumber) {
+          const q = query(collection(db, `transaction_data_${currentYear}`), where('uid', '==', user.uid), where('account_number', '==', currentAccountNumber));
+          const querySnapshot = await getDocs(q);
+          const transactionList = querySnapshot.docs.map(doc => doc.data());
+          setData(transactionList[0]);
+        }
       } catch (error) {
         console.error('Error getting bank statement document:', error);
       }
     };
 
-    const auth = getAuth();
-    if (auth) {
-      const user = auth.currentUser;
-      if (user !== null) {
-        getYearlyTransactions();
-      }
+    getYearlyTransactions();
+  }, [currentYear, currentAccountNumber, user.uid]);
+
+  const handleDropdownChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selected = event.target.value;
+    const selectedOption = accountOptions.find(option => option.alias === selected);
+    if (selectedOption) {
+      setSelectedAlias(selected);
+      setCurrentAccountNumber(selectedOption.accountNumber);
+      setData(null); // Reset data when dropdown changes
     }
-    console.log("new");
-  }, [currentYear]);
+  };
 
   return (
     <div>
@@ -75,17 +85,26 @@ export function Dashboard(props: DashboardProps) {
             onClick={() => setViewMode('monthly')}
           >Monthly</button>
         </div>
-        <select className={styles.accountDropdown}>
-          {aliases.map((alias, index) => (
-          <option key={index} value={alias}>
-            {alias}
-          </option>
+        <select 
+          className={styles.accountDropdown}
+          value={selectedAlias}
+          onChange={handleDropdownChange}
+        >
+          {accountOptions.map((option, index) => (
+            <option key={index} value={option.alias}>
+              {option.alias}
+            </option>
           ))}
         </select>
       </div>
 
       <div>
-        {viewMode === 'all' ? <AllTransactionsView /> : <MonthlyTransactionsView account = {currentAccountNumber} data = {Data}/>}
+        {viewMode === 'monthly' && Data ? 
+          (<MonthlyTransactionsView account={currentAccountNumber} data={Data} />) : 
+        viewMode === 'all' && Data ? 
+          (<AllTransactionsView />) : 
+        (<div className={styles.loadScreen}>Loading...</div>)
+      }
       </div>
     </div>
   );
--- shared/budgie-components/src/lib/monthly-transactions-view/MonthlyTransactionsView.tsx
+++ shared/budgie-components/src/lib/monthly-transactions-view/MonthlyTransactionsView.tsx
@@ -11,6 +11,8 @@ import {
   addDoc,
   setDoc,
   updateDoc,
+  query, 
+  where
 } from 'firebase/firestore';
 import { db } from '../../../../../apps/budgie-app/firebase/clientApp';
 import { getAuth } from 'firebase/auth';
@@ -19,7 +21,10 @@ import '../../root.css';
 import { json } from 'stream/consumers';
 import Metrics from '../metrics/Metrics'; // Import the Metrics component
 
-export interface MonthlyTransactionsViewProps {}
+export interface MonthlyTransactionsViewProps {
+  account: string;
+  data: any;
+}
 
 export function MonthlyTransactionsView(props: MonthlyTransactionsViewProps) {
   const [balance, setBalance] = useState(0);
@@ -29,8 +34,6 @@ export function MonthlyTransactionsView(props: MonthlyTransactionsViewProps) {
   const [currentMonth, setCurrentMonth] = useState(new Date());
   const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
   const [Data, setData] = useState<any>(null);
-  const [showMetrics, setShowMetrics] = useState(false); // State to control modal visibility
-  const [viewMode, setViewMode] = useState('monthly');
   const user = useContext(UserContext);
 
   interface Transaction {
@@ -74,62 +77,6 @@ export function MonthlyTransactionsView(props: MonthlyTransactionsViewProps) {
     return date.toLocaleString('default', { month: 'long', year: 'numeric' });
   };
 
-  function getUniqueYearMonths(DataLines: string[]): Record<string, string[]> {
-    const yearMonthsRecord: Record<string, Set<string>> = {};
-
-    for (const line of DataLines) {
-      const [date] = line.split(',');
-      const [year, month] = date.split('/');
-      const yearMonth = `${year}/${month}`;
-
-      if (!yearMonthsRecord[year]) {
-        yearMonthsRecord[year] = new Set();
-      }
-
-      yearMonthsRecord[year].add(yearMonth);
-    }
-
-    const result: Record<string, string[]> = {};
-
-    for (const year in yearMonthsRecord) {
-      result[year] = Array.from(yearMonthsRecord[year]);
-    }
-
-    return result;
-  }
-
-  function getSeparateYearMonthsAsTransactionObjects(
-    DataLines: string[]
-  ): Record<string, Transaction[]> {
-    const linesByYearMonth: Record<string, Transaction[]> = {};
-
-    for (const line of DataLines) {
-      const [date, amountStr, balanceStr, description] = line
-        .split(',')
-        .map((part) => part.trim());
-      const [year, month] = date.split('/');
-      const yearMonth = `${year}/${month}`;
-      const amount: number = parseFloat(amountStr);
-      const balance: number = parseFloat(balanceStr);
-
-      const transaction: Transaction = {
-        date,
-        amount,
-        balance,
-        description,
-        category: '', // Initialize category as an empty string
-      };
-
-      if (!linesByYearMonth[yearMonth]) {
-        linesByYearMonth[yearMonth] = [];
-      }
-
-      linesByYearMonth[yearMonth].push(transaction);
-    }
-
-    return linesByYearMonth;
-  }
-
   const monthNames = [
     'january',
     'february',
@@ -145,194 +92,13 @@ export function MonthlyTransactionsView(props: MonthlyTransactionsViewProps) {
     'december',
   ];
 
-  function getMonthName(month: string): string {
-    const monthIndex = parseInt(month, 10) - 1; // Months are zero-based
-    if (monthIndex >= 0 && monthIndex < 12) {
-      return monthNames[monthIndex];
-    } else {
-      throw new Error('Invalid month value');
-    }
-  }
-
-  function isDuplicate(
-    transaction1: Transaction,
-    transaction2: Transaction
-  ): boolean {
-    return (
-      transaction1.date === transaction2.date &&
-      transaction1.amount === transaction2.amount &&
-      transaction1.balance === transaction2.balance &&
-      transaction1.description === transaction2.description
-    );
-  }
-
-  async function MergeTransactions(
-    YearMonthLinesRecord: Record<string, Transaction[]>,
-    UniqueYearMonths: Record<string, string[]>
-  ) {
-    //determine merged record
-    for (const Year in UniqueYearMonths) {
-      let Merged: Record<string, Transaction[]> = {};
-      const YearMonths: string[] = UniqueYearMonths[Year];
-
-      //check if doc exists for year by retrieving it
-      const docRef = doc(db, `transaction_data_${Year}`, `${user.uid}`);
-      const docSnap = await getDoc(docRef);
-      if (docSnap.exists()) {
-        for (const YearMonth of YearMonths) {
-          let [Y, Month] = YearMonth.split('/');
-          Month = getMonthName(Month);
-          if (docSnap.data()[Month]) {
-            //month already contains data
-            const Incoming: Transaction[] = JSON.parse(docSnap.data()[Month]);
-            const Outgoing: Transaction[] = YearMonthLinesRecord[YearMonth];
-            //merge data duplicate removal
-            const filteredOutgoing = Outgoing.filter(
-              (outgoingTransaction) =>
-                !Incoming.some((incomingTransaction) =>
-                  isDuplicate(outgoingTransaction, incomingTransaction)
-                )
-            );
-
-            if (filteredOutgoing.length != 0) {
-              //there are some non duplicates merge and sort and update
-              const combinedTransactions = [...Incoming, ...filteredOutgoing];
-              combinedTransactions.sort((a, b) => {
-                const dateA = new Date(a.date);
-                const dateB = new Date(b.date);
-
-                return dateB.getTime() - dateA.getTime();
-              });
-              const TransactionString = JSON.stringify(combinedTransactions);
-              try {
-                const Ref = doc(db, `transaction_data_${Y}`, `${user.uid}`);
-                await updateDoc(Ref, {
-                  [Month]: TransactionString,
-                });
-              } catch (error) {
-                console.log(error);
-              }
-            }
-          } else {
-            //empty month can just merge
-            const Outgoing: Transaction[] = YearMonthLinesRecord[YearMonth];
-            Outgoing.sort((a, b) => {
-              const dateA = new Date(a.date);
-              const dateB = new Date(b.date);
-
-              return dateB.getTime() - dateA.getTime();
-            });
-            const TransactionString = JSON.stringify(Outgoing);
-            try {
-              const Ref = doc(db, `transaction_data_${Y}`, `${user.uid}`);
-              await updateDoc(Ref, {
-                [Month]: TransactionString,
-              });
-            } catch (error) {
-              console.log(error);
-            }
-          }
-        }
-        //call categorize function
-        const functions = getFunctions();
-        const categoriseExpenses = httpsCallable(
-          functions,
-          'categoriseExpenses'
-        );
-        console.log('first run');
-        categoriseExpenses({ year: Year });
-      } else {
-        //documents do not exist for this year can safely add to merged
-        for (const YearMonth of YearMonths) {
-          Merged[YearMonth] = YearMonthLinesRecord[YearMonth];
-        }
-        // TODO: sort merged on date
-        for (const YearMonth in Merged) {
-          const MonthlyTransactions: Transaction[] = Merged[YearMonth];
-          MonthlyTransactions.sort((a, b) => {
-            const dateA = new Date(a.date);
-            const dateB = new Date(b.date);
-
-            return dateB.getTime() - dateA.getTime();
-          });
-        }
-        //upload merged!document does not exist must first set then update
-        let first = true;
-        for (const YearMonth in Merged) {
-          let [year, month] = YearMonth.split('/');
-          const TransactionString = JSON.stringify(Merged[YearMonth]);
-          const TransactionMonthString = getMonthName(month);
-          if (first) {
-            first = false;
-            await setDoc(doc(db, `transaction_data_${year}`, `${user.uid}`), {
-              [TransactionMonthString]: TransactionString,
-            });
-          } else {
-            try {
-              const Ref = doc(db, `transaction_data_${year}`, `${user.uid}`);
-              await updateDoc(Ref, {
-                [TransactionMonthString]: TransactionString,
-              });
-            } catch (error) {
-              console.log(error);
-            }
-          }
-        }
-        //call categorize function
-        const functions = getFunctions();
-        const categoriseExpenses = httpsCallable(
-          functions,
-          'categoriseExpenses'
-        );
-        console.log('first run');
-        categoriseExpenses({ year: Year });
-      }
-    }
-    setTransactions([]);
-  }
-
-  const handleCSVUpload = async (file: File) => {
-    const reader = new FileReader();
-    reader.onload = async (event) => {
-      const content = event.target?.result as string;
-      const lines = content
-        .split('\n')
-        .map((line) => line.trim())
-        .filter((line) => line);
-
-      //extract transaction lines
-      const HeaderLine = lines.find((line) => line.startsWith('Date'));
-      if (HeaderLine) {
-        const DataLines = lines.slice(
-          lines.indexOf(HeaderLine) + 1,
-          lines.length
-        );
-        const YearMonthLinesRecord: Record<string, Transaction[]> =
-          getSeparateYearMonthsAsTransactionObjects(DataLines);
-        const UniqueYearMonths: Record<string, string[]> =
-          getUniqueYearMonths(DataLines);
-        await MergeTransactions(YearMonthLinesRecord, UniqueYearMonths);
-      } else {
-        //error incorrect csv format
-      }
-    };
-    reader.readAsText(file);
-  };
-
   useEffect(() => {
-    const getBankStatementsByUserId = async (userId: string) => {
+    const getYearlyTransactions = async () => {
       try {
-        const collectionName = `transaction_data_${currentYear}`;
-        const docRef = doc(db, collectionName, userId);
-        const docSnap = await getDoc(docRef);
-
-        if (docSnap.exists()) {
-          const data = docSnap.data();
-          // Process the document data here
-          setData(data);
-        } else {
-          console.log('No such document!');
-        }
+        const q = query(collection(db, `transaction_data_${currentYear}`), where('uid', '==', user.uid), where('account_number', '==', props.account));
+        const querySnapshot = await getDocs(q);
+        const transactionList = querySnapshot.docs.map(doc => doc.data());
+        setData(transactionList[0]);
       } catch (error) {
         console.error('Error getting bank statement document:', error);
       }
@@ -342,22 +108,32 @@ export function MonthlyTransactionsView(props: MonthlyTransactionsViewProps) {
     if (auth) {
       const user = auth.currentUser;
       if (user !== null) {
-        getBankStatementsByUserId(user.uid);
+        getYearlyTransactions();
       }
     }
-  }, [currentYear, transactions]);
+    //console.log("new");
+  }, [currentYear]);
 
   useEffect(() => {
-    if (Data !== null) {
+    if (Data === null) {
+      //console.log("No");
+    }
+    else 
+    {
       display();
     }
   }, [Data]);
+
+  useEffect(() => {
+    setData(props.data);
+  }, []);
+
 
   const display = async () => {
     const month = currentMonth
       .toLocaleString('default', { month: 'long' })
       .toLocaleLowerCase();
-    if (Data[month] === undefined) {
+    if (Data === undefined || Data[month] === undefined || Data[month] === null) {
       setTransactions([]);
       setBalance(0);
       setMoneyIn(0);
@@ -376,7 +152,7 @@ export function MonthlyTransactionsView(props: MonthlyTransactionsViewProps) {
         .reduce((acc: any, transaction: { amount: any; }) => acc + transaction.amount, 0);
 
       setMoneyIn(moneyInTotal);
-      setMoneyOut(Math.abs(moneyOutTotal)); // moneyOut should be positive for display
+      setMoneyOut(Math.abs(moneyOutTotal)); // money out should be positive for display
     }
   };
 
@@ -441,22 +217,10 @@ export function MonthlyTransactionsView(props: MonthlyTransactionsViewProps) {
       minimumFractionDigits: 2,
     });
     return formatter.format(value);
-  };
-  
+  };  
 
   return (
     <div className={styles.mainPage}>
-      
-      {/* <button
-        onClick={() => setShowMetrics(true)}
-        className={styles.metricsButton}
-      >
-        View Metrics
-      </button>
-
-      {showMetrics && <Metrics onClose={() => setShowMetrics(false)} />} */}
-
-
       <div className={styles.header}>
         <div className={styles.monthNavigation}>
           <button className={styles.navButton} onClick={handlePrevMonth}>
@@ -484,9 +248,6 @@ export function MonthlyTransactionsView(props: MonthlyTransactionsViewProps) {
             </span>
           </button>
         </div>
-
-        <UploadStatementCSV onFileUpload={handleCSVUpload} />
-
             <div className={styles.balanceInfo}>
               Balance:  {formatCurrency(balance)}
               <p className={styles.moneyInfo}>Money In:  {formatCurrency(moneyIn)}</p>
@@ -536,7 +297,7 @@ export function MonthlyTransactionsView(props: MonthlyTransactionsViewProps) {
                           <option value="Utilities">Utilities</option>
                           <option value="Medical Aid">Medical Aid</option>
                           <option value="Other">Other</option>
-                        </select>
+                      </select>
                     </div>
                 </div>
               ))}
