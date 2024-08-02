32b827555b1cf707aa2c652474e0171b22273c13
ArrJayOh <160711869+RebeccaOosthuizen@users.noreply.github.com>
2024-08-02 19:39:36 +0200

    Loading screen

--- shared/budgie-components/src/lib/dashboard/Dashboard.tsx
+++ shared/budgie-components/src/lib/dashboard/Dashboard.tsx
@@ -13,17 +13,27 @@ export interface DashboardProps {}
 
 export function Dashboard(props: DashboardProps) {
 
-  const [viewMode, setViewMode] = useState('monthly');
   const user = useContext(UserContext);
-  const [accountOptions, setAccountOptions] = useState<{ alias: string, accountNumber: string }[]>([]);
-  const [currentAccountNumber, setCurrentAccountNumber] = useState<string>('');
-  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
   const [Data, setData] = useState<any>(null);
+
+  // Yearly or monthly view
+  const [viewMode, setViewMode] = useState('monthly');
+  
+  // Default year is the current year
+  const currentYear = new Date().getFullYear();
+
+  // Current user's available accounts
+  const [accountOptions, setAccountOptions] = useState<{ alias: string, accountNumber: string }[]>([]);
+  
+  // Alias and account number of the currently selected account
   const [selectedAlias, setSelectedAlias] = useState<string>('');
+  const [currentAccountNumber, setCurrentAccountNumber] = useState<string>('');
+
+  // Set of years for which user has transactions
   const [yearsWithData, setYearsWithData] = useState<number[]>([0]);
 
   useEffect(() => {
-    const fetchAliases = async () => {
+    const fetchAccounts = async () => {
       try {
         const q = query(collection(db, 'accounts'), where('uid', '==', user.uid));
         const querySnapshot = await getDocs(q);
@@ -42,7 +52,7 @@ export function Dashboard(props: DashboardProps) {
       }
     };
 
-    fetchAliases();
+    fetchAccounts();
   }, []);
 
   useEffect(() => {
@@ -76,8 +86,7 @@ export function Dashboard(props: DashboardProps) {
             years.push(year);
           }
         }  
-        setYearsWithData(years);
-        
+        setYearsWithData(years);        
       } catch (error) {
         console.error('Error fetching years with data:', error);
       }
@@ -86,13 +95,13 @@ export function Dashboard(props: DashboardProps) {
     fetchAvailableYears();
   }, [currentAccountNumber]);
 
-  const handleDropdownChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleAccountDropdownChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selected = event.target.value;
     const selectedOption = accountOptions.find(option => option.alias === selected);
     if (selectedOption) {
       setSelectedAlias(selected);
       setCurrentAccountNumber(selectedOption.accountNumber);
-      setData(null); // Reset data when dropdown changes
+      setData(null);
     }
   };
 
@@ -107,6 +116,7 @@ export function Dashboard(props: DashboardProps) {
           >
               Yearly
           </button>
+
           <button
             className={`${styles.button} ${viewMode === 'monthly' ? styles.activeButton : ''}`}
             onClick={() => setViewMode('monthly')}
@@ -114,10 +124,11 @@ export function Dashboard(props: DashboardProps) {
             Monthly
           </button>
         </div>
+
         <select 
           className={styles.accountDropdown}
           value={selectedAlias}
-          onChange={handleDropdownChange}
+          onChange={handleAccountDropdownChange}
         >
           {accountOptions.map((option, index) => (
             <option key={index} value={option.alias}>
@@ -129,10 +140,18 @@ export function Dashboard(props: DashboardProps) {
 
       <div>
         {viewMode === 'monthly' && Data && yearsWithData[0]!==0 ? 
-          (<MonthlyTransactionsView account={currentAccountNumber} data={Data} availableYears={yearsWithData}/>) : 
-        viewMode === 'all' && Data && yearsWithData[0]!==0? 
-          (<AllTransactionsView account={currentAccountNumber} availableYears={yearsWithData}/>) : 
-        (<div className={styles.loadScreen}>Loading...</div>)
+          (<MonthlyTransactionsView account={currentAccountNumber} data={Data} availableYears={yearsWithData}/>) 
+
+        : viewMode === 'all' && Data && yearsWithData[0]!==0? 
+          (<AllTransactionsView account={currentAccountNumber} availableYears={yearsWithData}/>) 
+
+        : (<div className={styles.loadScreen}>
+            <div className={styles.loaderContainer}>
+              <div className={styles.loader}></div>
+            </div> 
+            <div className={styles.loaderText}>Loading...</div>
+            
+          </div>)
         }
       </div>
     </div>
