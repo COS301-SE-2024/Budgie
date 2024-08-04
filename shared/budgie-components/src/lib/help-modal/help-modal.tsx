43f4eb7ee7d44af8f50d9fd9e37c19cba2536cd6
Zviko <u18050825@tuks.co.za>
2024-08-04 19:06:29 +0200

    help modal content

--- shared/budgie-components/src/lib/help-modal/help-modal.tsx
+++ shared/budgie-components/src/lib/help-modal/help-modal.tsx
@@ -21,7 +21,22 @@ export function HelpModal(props: HelpModalProps) {
             <h1 className="text-4xl font-bold mb-4">How can we help?</h1>
           </div>
         </section>
-        <section></section>
+        <section>
+          <div className="mb-6">
+            <h3 className="text-xl font-semibold mb-4 text-BudgieBlue">
+              How It Works
+            </h3>
+            <p className="mb-2 text-BudgieBlue">
+              Follow these simple steps to get started:
+            </p>
+            <ol className="list-decimal list-inside ml-4 text-BudgieBlue">
+              <li>Sign up for an account.</li>
+              <li>Link your financial accounts.</li>
+              <li>Add CSV files of your bank statements.</li>
+              <li>Start managing your finances with ease.</li>
+            </ol>
+          </div>
+        </section>
       </div>
     </>
   );
