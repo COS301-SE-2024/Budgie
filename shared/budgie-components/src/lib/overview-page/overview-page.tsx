f971dad3bfea800c480a6ec56e7e8e0d600d281f
kabeloChuene <68307899+kabeloChuene@users.noreply.github.com>
2024-08-05 11:08:58 +0200

    styling
    fixing content to show entire page width

--- shared/budgie-components/src/lib/overview-page/overview-page.tsx
+++ shared/budgie-components/src/lib/overview-page/overview-page.tsx
@@ -6,7 +6,7 @@ import { faMoneyBill, faBank, faChartPie, faHistory, faCalendarAlt, faListUl, fa
 import styles from './overview-page.module.css';
 import HealthBar from './HealthBar';
 import '../../root.css';
-import {getAccounts} from "../overview-page/overviewServices";
+import { getAccounts } from "../overview-page/overviewServices";
 export interface OverviewPageProps {}
 
 export function OverviewPage(props: OverviewPageProps) {
@@ -26,7 +26,7 @@ export function OverviewPage(props: OverviewPageProps) {
     alert("alert")
     async function someFunction() {
       const account = await getAccounts();
-      if(account.length>0){
+      if (account.length > 0) {
         setAccounts(account);
         setShowData(true);
       }
@@ -34,9 +34,6 @@ export function OverviewPage(props: OverviewPageProps) {
 
     someFunction();
   }, [showData])
- 
-
-
 
   const [isDarkMode, setIsDarkMode] = useState(true);
 
@@ -79,126 +76,128 @@ export function OverviewPage(props: OverviewPageProps) {
 
   const hasAccounts = showData;
 
-    return (
-        <div className={styles.mainPage}>
-            <div className={styles.header}>
-                {/* Your header content here */}
-            </div>
+  return (
+    <div className={styles.mainPage}>
+      <div className={styles.header}>
+        {/* Your header content here */}
+      </div>
 
-            <div className={styles.accountStatus}>
-                {hasAccounts ? (
-                  <div className={`${styles.metricsContainer} ${isDarkMode ? styles.dark : styles.light}`}>
-                  <button className={styles.toggleButton} onClick={toggleTheme}>
-                    {isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
-                  </button>
-                  <div className={styles.chartContainer}>
-                    <div className={styles.chartItem}>
-                      <div className={styles.chartTitleContainer}>
-                        <h3 className={styles.chartTitle}>Net Worth Over Time</h3>
-                      </div>
-                      <LineChart width={650} height={300} data={spendingData}>
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="name" tick={{ fill: isDarkMode ? 'white' : 'black' }} />
-                        <YAxis tick={{ fill: isDarkMode ? 'white' : 'black' }} />
-                        <Tooltip />
-                        <Line type="monotone" dataKey="value" stroke="#8884d8" />
-                      </LineChart>
-                    </div>
-                    <div className={styles.chartItem}>
-                      <div className={styles.chartTitleContainer}>
-                        <h3 className={styles.chartTitle}>Spending by Category</h3>
-                      </div>
-                      <PieChart width={600} height={300}>
-                        <Pie
-                          data={categoryData}
-                          cx={250}
-                          cy={120}
-                          labelLine={false}
-                          outerRadius={100}
-                          fill="#8884d8"
-                          dataKey="value"
-                        >
-                          {categoryData.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={CATEGORY_COLORS[index % CATEGORY_COLORS.length]} />
-                          ))}
-                        </Pie>
-                        <Tooltip />
-                        <Legend />
-                      </PieChart>
-                    </div>
-                  </div>
-                  <div className={styles.gridContainer}>
-                    <div className={styles.gridItem}>
-                      <div className={styles.gridTitleContainer}>
-                        <FontAwesomeIcon icon={faMoneyBill} className={styles.icon} />
-                        <h2 className={styles.gridTitle}>Total Balance for Year</h2>
-                      </div>
-                      <p>Total Money in: R25 647.76</p>
-                      <p>Total Money out: R5 427.28</p>
-                    </div>
-                    <div className={styles.gridItem}>
-                      <div className={styles.gridTitleContainer}>
-                        <FontAwesomeIcon icon={faBank} className={styles.icon} />
-                        <h2 className={styles.gridTitle}>Current Accounts</h2>
-                      </div>
-                      <ul>
-                      {accounts.map(account => (
-                        <li>{account.alias}</li>
+      <div className={styles.accountStatus}>
+        {hasAccounts ? (
+          <div className={`${styles.metricsContainer} ${isDarkMode ? styles.dark : styles.light}`}>
+            <button className={styles.toggleButton} onClick={toggleTheme}>
+              {isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+            </button>
+            <div className={styles.chartContainer}>
+              <div className={styles.chartItem}>
+                <div className={styles.chartTitleContainer}>
+                  <h3 className={styles.chartTitle}>Net Worth Over Time</h3>
+                </div>
+                <div className={styles.chartContent}>
+                  <LineChart width={650} height={300} data={spendingData}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" tick={{ fill: isDarkMode ? 'white' : 'black' }} />
+                    <YAxis tick={{ fill: isDarkMode ? 'white' : 'black' }} />
+                    <Tooltip />
+                    <Line type="monotone" dataKey="value" stroke="#8884d8" />
+                  </LineChart>
+                </div>
+              </div>
+              <div className={styles.chartItem}>
+                <div className={styles.chartTitleContainer}>
+                  <h3 className={styles.chartTitle}>Spending by Category</h3>
+                </div>
+                <div className={styles.chartContent}>
+                  <PieChart width={600} height={300}>
+                    <Pie
+                      data={categoryData}
+                      cx={250}
+                      cy={120}
+                      labelLine={false}
+                      outerRadius={100}
+                      fill="#8884d8"
+                      dataKey="value"
+                    >
+                      {categoryData.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={CATEGORY_COLORS[index % CATEGORY_COLORS.length]} />
                       ))}
-                      </ul>
-                    </div>
-                    <div className={styles.gridItem}>
-                      <div className={styles.gridTitleContainer}>
-                        <FontAwesomeIcon icon={faHistory} className={styles.icon} />
-                        <h2 className={styles.gridTitle}>Last Transaction</h2>
-                      </div>
-                      <p>PURCH Uber Eats 400738******4299</p>
-                      <p>2024/06/22</p>
-                      <p>Category: Eating Out</p>
-                    </div>
-                    <div className={styles.gridItem}>
-                      <div className={styles.gridTitleContainer}>
-                        <FontAwesomeIcon icon={faCalendarAlt} className={styles.icon} />
-                        <h2 className={styles.gridTitle}>Upcoming Bills & Payments</h2>
-                      </div>
-                      <ul>
-                        <li>Car Instalment - Due in 15 days</li>
-                        <li>Bond Payment - Due in 27 days</li>
-                      </ul>
-                    </div>
-                    <div className={styles.gridItem}>
-                      <div className={styles.gridTitleContainer}>
-                        <FontAwesomeIcon icon={faListUl} className={styles.icon} />
-                        <h2 className={styles.gridTitle}>Budget Status</h2>
-                      </div>
-                      <p>Amount: R10 000.00</p>
-                      <div className={styles.budgetBar}>
-                        <div className={styles.budgetFill}></div>
-                      </div>
-                    </div>
-                    <div className={styles.gridItem}>
-                      <div className={styles.gridTitleContainer}>
-                        <FontAwesomeIcon icon={faBullseye} className={styles.icon} />
-                        <h2 className={styles.gridTitle}>Financial Goals Progress</h2>
-                      </div>
-                      <p>{progressMessage}</p>
-                      <HealthBar progress={financialGoalsProgress} />
-                    </div>
-                  </div>
+                    </Pie>
+                    <Tooltip />
+                    <Legend />
+                  </PieChart>
                 </div>
-                ) : (
-                <div className={styles.centeredMessage}>
-                    <p>You have not created<br/>
-                    any accounts <br/><br/>
-                    Head to the accounts<br/>
-                    section to create your first account</p>
-                </div>
-                    
-                )}
+              </div>
             </div>
-        </div>
-    );
-
+            <div className={styles.gridContainer}>
+              <div className={styles.gridItem}>
+                <div className={styles.gridTitleContainer}>
+                  <FontAwesomeIcon icon={faMoneyBill} className={styles.icon} />
+                  <h2 className={styles.gridTitle}>Total Balance for Year</h2>
+                </div>
+                <p>Total Money in: R25 647.76</p>
+                <p>Total Money out: R5 427.28</p>
+              </div>
+              <div className={styles.gridItem}>
+                <div className={styles.gridTitleContainer}>
+                  <FontAwesomeIcon icon={faBank} className={styles.icon} />
+                  <h2 className={styles.gridTitle}>Current Accounts</h2>
+                </div>
+                <ul>
+                  {accounts.map(account => (
+                    <li key={account.uid}>{account.alias}</li>
+                  ))}
+                </ul>
+              </div>
+              <div className={styles.gridItem}>
+                <div className={styles.gridTitleContainer}>
+                  <FontAwesomeIcon icon={faHistory} className={styles.icon} />
+                  <h2 className={styles.gridTitle}>Last Transaction</h2>
+                </div>
+                <p>PURCH Uber Eats 400738******4299</p>
+                <p>2024/06/22</p>
+                <p>Category: Eating Out</p>
+              </div>
+              <div className={styles.gridItem}>
+                <div className={styles.gridTitleContainer}>
+                  <FontAwesomeIcon icon={faCalendarAlt} className={styles.icon} />
+                  <h2 className={styles.gridTitle}>Upcoming Bills & Payments</h2>
+                </div>
+                <ul>
+                  <li>Car Instalment - Due in 15 days</li>
+                  <li>Bond Payment - Due in 27 days</li>
+                </ul>
+              </div>
+              <div className={styles.gridItem}>
+                <div className={styles.gridTitleContainer}>
+                  <FontAwesomeIcon icon={faListUl} className={styles.icon} />
+                  <h2 className={styles.gridTitle}>Budget Status</h2>
+                </div>
+                <p>Amount: R10 000.00</p>
+                <div className={styles.budgetBar}>
+                  <div className={styles.budgetFill}></div>
+                </div>
+              </div>
+              <div className={styles.gridItem}>
+                <div className={styles.gridTitleContainer}>
+                  <FontAwesomeIcon icon={faBullseye} className={styles.icon} />
+                  <h2 className={styles.gridTitle}>Financial Goals Progress</h2>
+                </div>
+                <p>{progressMessage}</p>
+                <HealthBar progress={financialGoalsProgress} />
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className={styles.centeredMessage}>
+            <p>You have not created<br />
+              any accounts <br /><br />
+              Head to the accounts<br />
+              section to create your first account</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default OverviewPage;
